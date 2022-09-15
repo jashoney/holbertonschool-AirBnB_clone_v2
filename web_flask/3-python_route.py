f5de40664714338671e0975610b2a4302d89f51b
jashoney <4479@holbertonschool.com>
2022-09-15 12:26:29 +1000

    added task 3

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""
+    starts a web application using Flask
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    """returns Hello HBNB!"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """returns HBNB"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def ctext(text):
+    """returns C followed by some text with spaces not _ """
+    return 'C ' + text.replace("_", " ")
+
+
+@app.route('/python/', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def ptext(text="is cool"):
+    """returns Python followed by some text with spaces not _
+       default text is cool
+    """
+    return 'Python ' + text.replace("_", " ")
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
